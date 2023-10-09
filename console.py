5a2f69c514be97e67477ac5478d2edd24007c59a
Selim <mamo31st@gmail.com>
2023-10-10 01:07:28 +0300

    add simple implementation for console.py

---
+++ console.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+
+"""Module that defines main logic of the command interpreter"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class that represents the HBNB command line interpreter"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
